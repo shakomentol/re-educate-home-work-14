35a5a1669bbc132adb528e34fd8441ff4885b25e
shako metonidze <120588118+shakomentol@users.noreply.github.com>
2023-01-27 09:35:51 -0800

    create app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+let personsName = [
+    {
+        name: "shako",
+        age: "21",
+        address: "g.tabidzis 3/5"
+    },
+    {
+        name: "salome",
+        age: "18",
+        address: "saguramo"
+    },
+    {
+        name: "saba",
+        age: "22",
+        address: "avlabari"
+    },
+];
+console.log ('my name is ${personsName[0].name}, my age is ${personsName[0].age}, my address is ${personsName[0].address}');
