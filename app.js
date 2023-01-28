fff9adb826d468c31d5261290565690a03c4ae8f
shako metonidze <120588118+shakomentol@users.noreply.github.com>
2023-01-28 09:13:51 -0800

    update app.js

--- app.js
+++ app.js
@@ -16,3 +16,8 @@ let personsName = [
     },
 ];
 console.log ('my name is ${personsName[0].name}, my age is ${personsName[0].age}, my address is ${personsName[0].address}');
+let personsName = "salome"
+let number = 19;
+if (personsName [1].age < number) {
+    console.log (i am a teenager);
+}
