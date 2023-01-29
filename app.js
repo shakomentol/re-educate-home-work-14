10c4b85827b45df53b1b70973c2bdd033d39cf45
shako metonidze <120588118+shakomentol@users.noreply.github.com>
2023-01-29 08:39:26 -0800

    update jss home work

--- app.js
+++ app.js
@@ -15,9 +15,55 @@ let personsName = [
         address: "avlabari"
     },
 ];
-console.log ('my name is ${personsName[0].name}, my age is ${personsName[0].age}, my address is ${personsName[0].address}');
-let personsName = "salome"
-let number = 19;
-if (personsName [1].age < number) {
-    console.log (i am a teenager);
+
+let index = 0
+console.log ('my name is ${personsName[index].name}  my age is ${personsName[index].age} my address is ${personsName[index].address}');
+let numbers = [18,21,22,34,24];
+console.log (numbers[0] + numbers[1] + numbers[2] + numbers[3] + numbers[4]);
+
+if (personsName [1].age < 19) {
+    console.log ("i am a teenager")
+} else {
+    console.log ("i am an adult")
+}
+
+let s = 14;
+switch (s) {
+    case 14:
+        console.log ("this is fourteen");
+        break;
+    case 7:
+        console.log ("this is seven");
+        break;
+    default:
+        console.log ("this is another number");
+        break;       
+}
+
+const weekDays = new Date().getDate; 
+switch (weekDays) {
+    case 0:
+        console.log("sunday");
+        break;
+    case 1:
+        console.log("monday");
+         break;
+    case 2:
+        console.log("tuesday");
+        break;
+    case 3:
+        console.log("wednesday");
+        break;
+    case 4: 
+        console.log("thursday");
+        break;
+    case 5:
+        console.log("friday");
+        break;
+    case 6:
+        console.log("saturday");
+        break;
+    default:
+        console.log("wrong day");
+        break;                         
 }
